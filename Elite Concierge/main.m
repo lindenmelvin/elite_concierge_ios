852f623c8fa042bfc2bac06a7536a500e0ae41c7
Linden Melvin <linden.melvin@gmail.com>
2013-06-24 19:03:47 -0700

    Initial Commit

---
+++ Elite Concierge/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Elite Concierge
+//
+//  Created by Linden Melvin on 6/24/13.
+//  Copyright (c) 2013 Linden Melvin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
